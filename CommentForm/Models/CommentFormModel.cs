e0ec6cd5ebe36789981f785e48ece0ac96caaa50
Jamie Burke <jamieburke1020@gmail.com>
2017-02-07 14:31:01 -0500

    this is the second commit with properties from Model class

--- CommentForm/Models/CommentFormModel.cs
+++ CommentForm/Models/CommentFormModel.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations; //this or the other one is fine
 using System.Linq;
 using System.Web;
+
 
 namespace CommentForm.Models
 {
     public class CommentFormModel
-    {
+    {   
+        //Our ID keeps track of each instance
+        [Key]
+        public int ID { get; set; }   //need to set this up as a property; the other form of properites with the explicit get set and return value can be used but not necessary
+
+        public string Name { get; set; }
+        public string Comment { get; set; }
+        public int Priority { get; set; }
     }
 }
